f96a019305667c02fd626e65dce7f1ddda217a96
hugo <hugovarellaa@gmail.com>
2021-03-30 20:33:01 -0300

    chore: Config eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Text } from "@ignite-ui/react";
 import Head from "next/head";
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>ola mundo</h1>
+        <Text>ola mundo</Text>
       </main>
     </>
   );
