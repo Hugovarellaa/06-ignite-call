14da89897ae07047e328f8b6eb8ef732dcb4aa4b
hugo <hugovarellaa@gmail.com>
2021-04-04 21:23:58 -0300

    chore: Config Styles Container Home

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,6 +1,16 @@
 import { styled } from '@ignite-ui/react'
 
-export const Container = styled('div', {})
+export const Container = styled('div', {
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
+  height: '100vh',
+  marginLeft: 'auto',
+
+  display: 'flex',
+  alignItems: 'center',
+
+  gap: '$20',
+  overflow: 'hidden',
+})
 
 export const Hero = styled('div', {})
 
