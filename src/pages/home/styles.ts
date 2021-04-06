9d45c706999676ce663028bfa193afb91d19cbe0
hugo <hugovarellaa@gmail.com>
2021-04-05 21:41:15 -0300

    chore: Config Styles Container Hero

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,17 +1,29 @@
-import { styled } from '@ignite-ui/react'
+import { Heading, Text, styled } from '@ignite-ui/react'
 
 export const Container = styled('div', {
   maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
-  height: '100vh',
   marginLeft: 'auto',
-
+  height: '100vh',
   display: 'flex',
   alignItems: 'center',
-
   gap: '$20',
-  overflow: 'hidden',
 })
 
-export const Hero = styled('div', {})
+export const Hero = styled('div', {
+  maxWidth: 480,
+  padding: '0 $10',
+  paddingRight: 0,
+
+  [`${Heading}`]: {
+    '@media(max-width: 600px)': {
+      fontSize: '$6xl',
+    },
+  },
+
+  [`${Text}`]: {
+    maskType: '$2',
+    color: '$gray200',
+  },
+})
 
 export const Preview = styled('div', {})
