cb6d94e185a21811d72747026afe8eb3ffccba09
hugo <hugovarellaa@gmail.com>
2021-04-09 22:21:02 -0300

    chore: Validation username with zod

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from '@phosphor-icons/react'
 import { useForm } from 'react-hook-form'
@@ -6,13 +7,19 @@ import { z } from 'zod'
 import { Form } from './styles'
 
 const claimUsernameFormSchema = z.object({
-  username: z.string(),
+  username: z
+    .string()
+    .min(3, 'O usuário precisar ter pelo menos 3 letras')
+    .regex(/^([a-z\\-]+)$/i)
+    .transform((username) => username.toLowerCase()),
 })
 
 type FormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-  const { register, handleSubmit } = useForm<FormData>()
+  const { register, handleSubmit } = useForm<FormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
 
   async function handleClaimUsername(data: FormData) {
     console.log(data)
