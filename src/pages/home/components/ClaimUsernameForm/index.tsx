6e1b961d6ed1678f0a560395f5e2656dd1ac2de5
hugo <hugovarellaa@gmail.com>
2021-04-09 22:14:40 -0300

    chore: Using useForm to React-Hook-Form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,13 +1,32 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Form } from './styles'
 
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type FormData = z.infer<typeof claimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<FormData>()
+
+  async function handleClaimUsername(data: FormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       {/* @ts-ignore */}
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario" />
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
       <Button type="submit" size="sm">
         Reservar
         <ArrowRight />
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,11 @@
 import { Heading, Text } from '@ignite-ui/react'
 import Head from 'next/head'
-import { Container, Hero, Preview } from './styles'
-
 import Image from 'next/image'
+
 import previewImage from '../../assets/calendar.png'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+
+import { Container, Hero, Preview } from './styles'
 
 export default function Home() {
   return (
