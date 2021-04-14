e21553ea469f28494f23377b45549f8c92f902c2
hugo <hugovarellaa@gmail.com>
2021-04-13 23:22:35 -0300

    chore: Styles page Register

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,9 +1,38 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Container, Form, Header } from './styles'
 
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisar ter pelo menos 3 letras' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter somente letras e hifens',
+    })
+    .transform((username) => username.toLowerCase()),
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisar ter pelo menos 3 letras' }),
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema>
+
 export default function Register() {
+  const { register, handleSubmit, formState } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  const { errors, isSubmitting } = formState
+
+  function handleRegister(data: RegisterFormData) {
+    console.log(data)
+  }
+
   return (
     <Container>
       <Header>
@@ -16,20 +45,24 @@ export default function Register() {
         <MultiStep size={4} currentStep={1} />
       </Header>
 
-      <Form as="form">
+      <Form as="form" onSubmit={handleSubmit(handleRegister)}>
         <label>
           <Text size="sm">Nome de usuário</Text>
           {/* @ts-ignore */}
-          <TextInput prefix="ignite.com/" placeholder="seu-usuário" />
+          <TextInput
+            prefix="ignite.com/"
+            placeholder="seu-usuário"
+            {...register('username')}
+          />
         </label>
 
         <label>
           <Text size="sm">Nome completo</Text>
           {/* @ts-ignore */}
-          <TextInput prefix="ignite.com/" placeholder="Seu nome" />
+          <TextInput placeholder="Seu nome" {...register('name')} />
         </label>
 
-        <Button type="submit">
+        <Button type="submit" disabled={isSubmitting}>
           Próximo passo
           <ArrowRight />
         </Button>
