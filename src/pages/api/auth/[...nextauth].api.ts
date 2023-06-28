287e1e035cc6812c2fe38611944f7ddb915794f4
hugo <hugovarellaa@gmail.com>
2023-06-28 16:21:19 -0300

    chore: Add callbacks

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -16,6 +16,21 @@ export const authOptions: NextAuthOptions = {
     }),
     // ...add more providers here
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl
+    },
+    async session({ session, user, token }) {
+      return session
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token
+    },
+  },
 }
 
 export default NextAuth(authOptions)
